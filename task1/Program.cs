0638e5b4053ab512cbfc8db00a4c1e38178cd54c
Egor <egordolgih@mail.ru>
2022-07-11 19:53:02 +0300

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Degree(int numA, int numB)
+{
+	int result = 1;
+	for (int i = 1; i <= numB; i++)
+	{
+		result = result * numA;
+	}
+	return result;
+}
+
+Console.WriteLine("Введите целое А ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int destination = Degree(numberA, numberB);
+Console.WriteLine("Число А в степени В равно " + destination);
